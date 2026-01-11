7e73fb113ed0d41ac2edbd91bc565b4c5e87c3e7
Ahmed Zulfiqar <ahmedzulfiqar.dev@gmail.com>
2026-01-11 13:50:31 +0500

    d

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,17 @@
 import { FlatCompat } from '@eslint/eslintrc'
- 
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 const compat = new FlatCompat({
-  // import.meta.dirname is available after Node.js v20.11.0
-  baseDirectory: import.meta.dirname,
+  baseDirectory: __dirname,
 })
- 
+
 const eslintConfig = [
-  ...compat.config({
-    extends: ['next'],
-    plugins: ['import'],
-  }),
+  ...compat.extends('next/core-web-vitals'),
+  ...compat.extends('next/typescript'),
   {
     rules: {
       'react/no-unescaped-entities': 'off',
@@ -17,17 +19,8 @@ const eslintConfig = [
       '@typescript-eslint/no-unused-vars': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
       'react-hooks/exhaustive-deps': 'off',
-      'import/no-unresolved': 'error',
-      'import/named': 'error',
-      'import/default': 'error',
-      'import/namespace': 'error',
-      'import/no-absolute-path': 'error',
-      'import/no-dynamic-require': 'error',
-      'import/no-self-import': 'error',
-      'import/no-cycle': 'error',
-      'import/no-useless-path-segments': 'error',
-    },
-  },
+    }
+  }
 ]
- 
+
 export default eslintConfig
